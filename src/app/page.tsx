f780ddbe7647727550da67db0c506f305a675bfb
capy-on-caffeine <arnav.anand2004@gmail.com>
2024-09-22 22:17:33 +0530

    tracks section draft finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import Tracks from '@/components/tracks/Tracks'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Tracks />
+    </div>
   )
 }
 
