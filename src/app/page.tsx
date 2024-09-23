74a3658d95bc0464a0b92c1696ed12483c72e612
capy-on-caffeine <arnav.anand2004@gmail.com>
2024-09-23 10:25:11 +0530

    schedule draft

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Schedule from '@/components/schedule/Schedule'
 import Tracks from '@/components/tracks/Tracks'
 import React from 'react'
 
@@ -5,6 +6,7 @@ const Home = () => {
   return (
     <div>
       <Tracks />
+      <Schedule />
     </div>
   )
 }
