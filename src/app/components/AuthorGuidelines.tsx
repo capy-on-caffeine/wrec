fc7462c05df52a60e7be4b022300d9a90647d091
janak varshney <varshneyjanak@gmail.com>
2025-03-22 21:08:46 +0530

    author guidelines changes

--- src/app/components/AuthorGuidelines.tsx
+++ src/app/components/AuthorGuidelines.tsx
@@ -4,7 +4,7 @@ const AuthorGuideline = () => {
         return (
                 <section className="p-8 bg-white text-gray-900 max-w-4xl mx-auto rounded-2xl shadow-lg">
                         <h1 className="text-4xl font-bold mb-6 text-center">
-                        <span className="text-pink-500">AUHTOR</span> GUIDELINES
+                                <span className="text-pink-500">AUHTOR</span> GUIDELINES
                         </h1>
 
 
@@ -27,9 +27,9 @@ const AuthorGuideline = () => {
                                         Formatting Instructions
                                 </h2>
                                 <ul className="list-disc list-inside space-y-2 text-gray-800">
+                                        <li>The paper length must be 6-10 pages. For additional pages (upto 2 pages), the authors have to pay Rs. 1000/- or $50 per page.</li>
                                         <li>Papers must be written in English.</li>
                                         <li>Use the Springer LNCS template (LaTeX or Word formats).</li>
-                                        <li>Page limit: 12-15 pages, including references and figures.</li>
                                         <li>All figures and tables should be clear, legible, and numbered.</li>
                                         <li>References must adhere to the Springer citation style.</li>
                                 </ul>
@@ -46,8 +46,8 @@ const AuthorGuideline = () => {
                                         based on originality, significance, technical quality, and clarity.
                                 </p>
                         </div>
-        
-                                {/* Publication */}     
+
+                        {/* Publication */}
                         <div className="mb-8">
                                 <h2 className="text-2xl font-semibold mb-3 text-pink-500">
                                         Publication
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ import KeynoteSpeakers from "./components/KeynoteSpeakers";
 import Committee from "@/components/committee/Committee";
 import Details from "./components/Details";
 import Marquee from "./components/Marquee";
-import AuthorGuideline from "./components/AuthorGuideline";
+import AuthorGuidelines from "./components/AuthorGuidelines";
 
 
 const Home = () => {
@@ -22,7 +22,7 @@ const Home = () => {
       <Marquee />
       <Details />
       <AboutUs />
-      <AuthorGuideline />
+      <AuthorGuidelines />
       <KeynoteSpeakers />
       <Tracks />
       {/* <Tutorials /> */}
