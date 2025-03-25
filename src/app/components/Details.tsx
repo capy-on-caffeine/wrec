e8ebd0d56a157d011fd2b055f79830f89b3b10d9
janak varshney <varshneyjanak@gmail.com>
2025-03-25 20:14:08 +0530

    date changes

--- src/app/components/Details.tsx
+++ src/app/components/Details.tsx
@@ -4,9 +4,9 @@ import React from "react";
 
 const Details = () => {
   const data = {
-    lastDate: "25 March, 2025",
-    initiationAccept: "31 March, 2025",
-    coursesReady: "5th April, 2025",
+    lastDate: "31st March, 2025",
+    initiationAccept: "5th April, 2025",
+    coursesReady: "8th April, 2025",
     lastAndReg: "8 April, 2025",
     conf: "18-20 April, 2025",
   };
